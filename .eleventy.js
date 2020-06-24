84dfb49ec144a034bf11c4929dd37665e1dac64f
Lars <larsmackenzie@gmail.com>
2020-06-24 17:16:27 -0400

    i have a blog

---
+++ .eleventy.js
@@ -0,0 +1,35 @@
+module.exports = config => {
+// Set directories to pass through to the dist folder
+config.addPassthroughCopy('./src/images/');
+
+// defines our sort function so we can use it
+const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+
+// Returns work items, sorted by display order
+config.addCollection('work', collection => {
+  return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
+});
+
+// Returns work items, sorted by display order then filtered by featured
+config.addCollection('featuredWork', collection => {
+  return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md')).filter(
+    x => x.data.featured
+  );
+});
+
+// Returns a collection of blog posts in reverse date order
+config.addCollection('blog', collection => {
+  return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
+});
+
+
+    return {
+        markdownTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',  
+      dir: {
+        input: 'src',
+        output: 'dist'
+      }
+    };
+  };
